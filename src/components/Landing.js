9736ee674460dd67193e53bea7cc4be6218fa9d2
Conor Souhrada <ConorSouhrada@gmail.com>
2018-03-26 15:42:30 -0600

    checkpoint-4 commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 
 const Landing = () => (
   <section className="Landing">
-    Landing page will go here
+
+      <h1 className="hero-title">Turn Up The Music!</h1>
+
+    <section className="selling-points">
+
+      <div className="point">
+        <h2 className="point-title">Choose Your Music.</h2>
+        <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+      </div>
+
+      <div className="point">
+        <h2 className="point-title">Unlimited, streaming, ad-free.</h2>
+        <p className="point-description">No arbitrary limits. No distractions.</p>
+      </div>
+
+      <div className="point">
+        <h2 className="point-title">Mobile enabled.</h2>
+        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+      </div>
+
+    </section>
+
   </section>
 );
 
