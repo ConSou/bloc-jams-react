c71afe0ee634612421957ed4142bfe29dc5e96c2
Conor Souhrada <ConorSouhrada@gmail.com>
2018-03-28 21:44:59 -0600

    assignment-6 commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 
-const Landing = () => (
-  <section className="Landing">
+class Landing extends Component {
+  render() {
+    return (
+      <section className="Landing">
 
-      <h1 className="hero-title">Turn Up The Music!</h1>
+          <h1 className="hero-title">Turn Up The Music!</h1>
 
-    <section className="selling-points">
+        <section className="selling-points">
 
-      <div className="point">
-        <h2 className="point-title">Choose Your Music.</h2>
-        <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-      </div>
+          <div className="point">
+            <h2 className="point-title">Choose Your Music.</h2>
+            <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+          </div>
 
-      <div className="point">
-        <h2 className="point-title">Unlimited, streaming, ad-free.</h2>
-        <p className="point-description">No arbitrary limits. No distractions.</p>
-      </div>
+          <div className="point">
+            <h2 className="point-title">Unlimited, streaming, ad-free.</h2>
+            <p className="point-description">No arbitrary limits. No distractions.</p>
+          </div>
 
-      <div className="point">
-        <h2 className="point-title">Mobile enabled.</h2>
-        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-      </div>
+          <div className="point">
+            <h2 className="point-title">Mobile enabled.</h2>
+            <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+          </div>
 
-    </section>
+        </section>
 
-  </section>
-);
+      </section>
+    );
+  }
+}
 
 export default Landing;
